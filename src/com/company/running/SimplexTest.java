af85dcbda8e891aed3c7947458023cb4e2ab5527
maximpopov11 <icecreamguy0@gmail.com>
2022-11-03 18:30:01 -0500

    Implement minimization test without changing constraints.

--- src/com/company/running/SimplexTest.java
+++ src/com/company/running/SimplexTest.java
@@ -2,24 +2,33 @@ package com.company.running;
 
 /**
  * Runs Simplex over specific parameters to test its usage.
+ * To run minmization over same input with constraints becoming <= but same values:
+ *  remove slack variables from tableau
+ *  multiply bottom row by -1
+ *  transpose values
+ *  multiply new bottom row by -1 (keeps the negatives in the bottom)
+ *  reintroduce slack variables into tableau
  */
 public class SimplexTest {
 
     public static void main(String[] args) {
-        int numConstraints = 2;
-        int numVariables = 2 * 2;
-        Simplex simplex = new Simplex(numConstraints, numVariables);
+        int numConstraints;
+        int numVariables;
+        Simplex simplex;
 
+        numConstraints = 2;
+        numVariables = 2 * 2;
+        simplex = new Simplex(numConstraints, numVariables);
         // maximizing: value = 78 at (6, 12)
         // function: 5x1 + 4x2
         // constraint 1: 3x1 + 5x2 <= 78
         // constraint 2: 4x1 + x2 <= 36
-        float[][] tableau = {
+        float[][] tableauMax = {
                 {3,  5,   1, 0,  78},
                 {4,  1,   0, 1,  36},
                 {-5, -4,  0, 0,  0},
         };
-        simplex.fillTable(tableau);
+        simplex.fillTable(tableauMax);
         while (true) {
             Simplex.ERROR result = simplex.compute();
             if(result == Simplex.ERROR.IS_OPTIMAL) {
@@ -32,6 +41,25 @@ public class SimplexTest {
             }
         }
 
+        // transpose matrix for minimization (constraints are now >=)
+        simplex = new Simplex(numConstraints, numVariables);
+        float[][] tableauMin = {
+                {3,   4,   1, 0, 5},
+                {5,   1,   0, 1, 4},
+                {-78, -36, 0, 0, 0},
+        };
+        simplex.fillTable(tableauMin);
+        while (true) {
+            Simplex.ERROR result = simplex.compute();
+            if(result == Simplex.ERROR.IS_OPTIMAL) {
+                System.out.println("Optimal Solution Tableau:");
+                simplex.print();
+                break;
+            } else if (result == Simplex.ERROR.UNBOUNDED) {
+                System.out.println("Unbounded!");
+                break;
+            }
+        }
     }
 
 }
