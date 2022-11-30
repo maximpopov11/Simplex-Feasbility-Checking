a2dc86f60bf8e993bee68c23cc62d0af099ef928
maximpopov11 <icecreamguy0@gmail.com>
2022-11-29 19:01:11 -0600

    Implement randomly generated tests. Runnable but unfinished: - Runs single test with all constraints <=, resulting in few inequalities defining domain and thus a much lesser need for Sign-Changing Simplex. - Sign-Changing Simplex contains a bug where it checks pre-initial BFS candidate values, potentially returning incorrect values and taking up more time.

--- src/com/company/running/Main.java
+++ src/com/company/running/Main.java
@@ -1,5 +1,7 @@
 package com.company.running;
 
+import java.util.Arrays;
+
 import static com.company.running.SimplexType.*;
 
 public class Main {
@@ -8,21 +10,107 @@ public class Main {
      * Default constraints are <= bounds. Implement >= bounds by multiplying both expected sides by -1.
      */
     public static void main(String[] args) {
+        //TODO: BUG: sign changing simplex checks before basic feasible solution is found
+        // (so checks intermediate infeasible solutions at start)
+        int NUM_VARIABLES = 5;
+        int NUM_INEQUALITIES = 50;
+        int DOMAIN_RADIUS = 1;
+        int NUM_TESTS = 10;
+        double[] functionCoefficients = generateFunctionCoefficients(NUM_VARIABLES);
+        runRandomTests(functionCoefficients, NUM_INEQUALITIES, DOMAIN_RADIUS, NUM_TESTS);
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        runRandomTests(functionCoefficients, NUM_INEQUALITIES, DOMAIN_RADIUS, NUM_TESTS);
+    }
 
-        double[][] constraintVariableCoefficients = {
-                {1, 0},
-                {0, 1},
-                {-1, 0},
-                {0, -1},
-        };
-        double[] constraintConstants = new double[]{2, 1, 1, 2};
-        double[] functionVariableCoefficients = new double[]{2, 1};
+    /**
+     * Generates function coefficients between -100 and 100.
+     * @param numVariables number of variables in function (= dimension).
+     * @return generated function.
+     */
+    private static double[] generateFunctionCoefficients(int numVariables) {
+        double[] function = new double[numVariables];
+        for (int i = 0; i < function.length; i++) {
+            function[i] = Math.random() * 200 - 100;
+        }
+        return function;
+    }
 
+    /**
+     * Generates constraint constants for inequalities.
+     * @param numVariables number of variables (dimension).
+     * @param numBoundaryConstraints number of boundary constraints (defining domain).
+     * @param numInequalities number of inequalities to generate.
+     * @return generated constraint coefficients.
+     */
+    private static double[][] generateConstraintCoefficients(int numVariables, int numBoundaryConstraints,
+                                                             int numInequalities) {
+        double[][] constraintCoefficients = new double[numInequalities][numVariables];
+        for (int i = 0; i < numVariables; i++) {
+            double[] boundaryConstraintPositive = new double[numVariables];
+            boundaryConstraintPositive[i] = 1;
+            double[] boundaryConstraintNegative = new double[numVariables];
+            boundaryConstraintNegative[i] = -1;
+            constraintCoefficients[2*i] = boundaryConstraintPositive;
+            constraintCoefficients[2*i+1] = boundaryConstraintNegative;
+        }
+        // We can prove that the set of all equations that fit inside the circle (or higher dimensional figure) defined
+        // by the given radius is equal to the set of all equations a^2 + b^2 + ... > 1 for d > 0 and when at least one
+        // coefficient ≠ 0.
+        // Thus, we generate a series of inequalities in a subset of the legal equations where the coefficients are all
+        // > 0.
+        for (int i = numBoundaryConstraints; i < constraintCoefficients.length; i++) {
+            double[] singleConstraintCoefficients = new double[numVariables];
+            for (int j = 0; j < singleConstraintCoefficients.length; j++) {
+                // find random value > 1 to ensure legality (losing possibility for some legal equations)
+                double coefficient = 0;
+                while (coefficient == 0) {
+                    coefficient = Math.random();
+                }
+                while (coefficient < 1) {
+                    coefficient *= 10;
+                }
+                // 50% chance to be negative
+                if (Math.random() < 0.5) {
+                    coefficient *= -1;
+                }
+                singleConstraintCoefficients[j] = coefficient;
+            }
+            constraintCoefficients[i] = singleConstraintCoefficients;
+        }
+        return constraintCoefficients;
+    }
+
+    /**
+     * Generates and runs random tests
+     * @param functionCoefficients coefficients of target function
+     * @param numBoundedInequalities number of inequalities to generate
+     * @param domainRadius radius of domain bounding circle/sphere/higher dimensional figure centered at origin. This
+     *                     must be positive.
+     */
+    private static void runRandomTests(double[] functionCoefficients, int numBoundedInequalities, double domainRadius,
+                                       int numTests) {
+        int numVariables = functionCoefficients.length;
+        int numBoundaryConstraints = 2 * numVariables;
+        int numInequalities = numBoundaryConstraints + numBoundedInequalities;
+        double[] constraintConstants = new double[numInequalities];
+        Arrays.fill(constraintConstants, domainRadius);
+        double[][] constraintCoefficients = generateConstraintCoefficients(numVariables, numBoundaryConstraints,
+                numInequalities);
+        // TODO: Run many different tests. Currently runs all <= thus has few bounds. Randomizing = likely infeasible.
+        Simplex simplex = new Simplex(constraintCoefficients, constraintConstants, functionCoefficients);
+        runTests(simplex);
+    }
+
+    /**
+     * Runs simplex variations on given simplex and gathers data
+     * @param simplex simplex (tableau) instance to run simplex variations over
+     */
+    private static void runTests(Simplex simplex){
         long startTime;
         long endTime;
         long totalTime;
-
-        Simplex simplex = new Simplex(constraintVariableCoefficients, constraintConstants, functionVariableCoefficients);
         System.out.println("Simplex:");
         startTime = System.nanoTime();
         simplex.run(SIMPLEX);
@@ -42,4 +130,5 @@ public class Main {
         totalTime = endTime - startTime;
         System.out.println("Time: " + totalTime);
     }
+
 }
